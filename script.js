26cd1778db1956d78882d7d533cd8c6d819e54ff
Gerardo Serrano <gerardoserrc@gmail.com>
2025-11-19 12:50:28 -0600

    feat: Se agregan comentarios y cambio de nombre a la variable i en el archivo script.js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-for (i = 0; i <= 1000; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
+// Reto: FizzBuzz desde el número 1 hasta 1000.
+
+/* Reglas:
+    - Si el número es divisible entre 3, imprimir "Fizz".
+    - Si el número es divisible entre 5, imprimir "Buzz".
+    - Si el número es divisible entre 3 y 5, imprimir "FizzBuzz".
+    - Si no es divisible entre ninguno, imprimir el número.
+*/
+
+// Inicialización de la variable para el ciclo.
+let numero = 0;
+
+//El ciclo for itera desde 1 hasta 1000.
+for (numero = 1; numero <= 1000; numero++) {
+  // Condicionales para verificar las reglas de FizzBuzz.
+  // La condición más específica (divisible entre 3 y 5) se verifica primero.
+  if (numero % 3 === 0 && numero % 5 === 0) {
     console.log("FizzBuzz");
-  } else if (i % 3 === 0) {
+  }
+  // Luego se verifican las siguientes condiciones.
+    else if (numero % 3 === 0) {
     console.log("Fizz");
-  } else if (i % 5 === 0) {
+  } else if (numero % 5 === 0) {
     console.log("Buzz");
   } else {
-    console.log(i);
+    console.log(numero);
   }
 }
 
-//i = prompt("Enter a number between 1 and 1000:");
