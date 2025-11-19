e5853f9932f3a54a36e2a55d36936f0a6a9cb42e
Gerardo Serrano <gerardoserrc@gmail.com>
2025-11-19 12:34:18 -0600

    feat: Actualización del archivo index.html

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-/*for (i = 0; i <= 1000; i++) {
-  console.log(i);
-} */
-
-i = prompt("Enter a number between 1 and 1000:");
-
-if (i % 3 === 0 && i % 5 === 0) {
-  console.log("FizzBuzz");
-} else if (i % 3 === 0) {
-  console.log("Fizz");
-} else if (i % 5 === 0) {
-  console.log("Buzz");
-} else {
-  console.log(i);
+for (i = 0; i <= 1000; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
 }
+
+//i = prompt("Enter a number between 1 and 1000:");
