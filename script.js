acdf24a6ca192ee56520e553f19c50aa7c4590cf
Gerardo Serrano <gerardoserrc@gmail.com>
2025-11-19 10:50:24 -0600

    feat: Implementación del condicional if para imprimir Fizz, Buzz o FizzBuzz en script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-for (i = 0; i <= 1000; i++) {
+/*for (i = 0; i <= 1000; i++) {
+  console.log(i);
+} */
+
+i = prompt("Enter a number between 1 and 1000:");
+
+if (i % 3 === 0 && i % 5 === 0) {
+  console.log("FizzBuzz");
+} else if (i % 3 === 0) {
+  console.log("Fizz");
+} else if (i % 5 === 0) {
+  console.log("Buzz");
+} else {
   console.log(i);
 }
